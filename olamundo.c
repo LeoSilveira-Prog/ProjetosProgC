9aca3c058b517d3b8e0a11a804d772295d6d562f
LeoSilveira-Prog <Leosilveira88@live.com>
2025-03-29 11:15:49 -0300

    Update olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(){
+    int idade, matricula;
+    float altura;
+    char nome[50];
+    
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("digite seu nome: \n");
+    scanf("%s", &nome);
+
+    printf("digite sua  matrícula: \n");
+    scanf("%d", &matricula);
+
+    printf("Nome do aluno: %s - Matrícula: %d \n", nome, matricula);
+    printf("Idade: %d - altura: %f", idade, altura);
+
+    return 0;
+
+}
